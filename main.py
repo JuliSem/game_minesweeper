1310ac18991d7e68ce78c93bc10e338fb9ffbe34
JuliSem <julia.sem295@yandex.ru>
2024-08-10 18:20:27 +0300

    creating barrier elements

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import shuffle
 
 class MyButton(tk.Button):
 
-    def __init__(self, master, x, y, number, *args, **kwargs):
+    def __init__(self, master, x, y, number=0, *args, **kwargs):
         super(MyButton, self).__init__(
             master,
             width=3,
@@ -25,18 +25,16 @@ class MineSweeper:
     window = tk.Tk()
     ROW = 4
     COLUMN = 4
-    MINES = 3
+    MINES = 10
 
     def __init__(self):
         self.buttons = []
-        count = 1
-        for i in range(MineSweeper.ROW):
+        for i in range(MineSweeper.ROW+2):
             temp = []
-            for j in range(MineSweeper.COLUMN):
-                btn = MyButton(MineSweeper.window, x=i, y=j, number=count)
+            for j in range(MineSweeper.COLUMN+2):
+                btn = MyButton(MineSweeper.window, x=i, y=j)
                 btn.config(command=lambda button=btn: self.click(button))
                 temp.append(btn)
-                count += 1
             self.buttons.append(temp)
 
     def click(self, clicked_button: MyButton):
@@ -54,15 +52,32 @@ class MineSweeper:
         clicked_button.config(state='disabled')
 
     def create_widgets(self):
-        for i in range(MineSweeper.ROW):
-            for j in range(MineSweeper.COLUMN):
+        for i in range(MineSweeper.ROW+2):
+            for j in range(MineSweeper.COLUMN+2):
                 btn = self.buttons[i][j]
                 btn.grid(row=i, column=j)
+
+    def open_all_buttons(self):
+        for i in range(MineSweeper.ROW+2):
+            for j in range(MineSweeper.COLUMN+2):
+                btn = self.buttons[i][j]
+                if btn.is_mine:
+                    btn.config(
+                        text='*',
+                        background='red',
+                        disabledforeground='black'
+                    )
+                else:
+                    btn.config(
+                        text=btn.number,
+                        disabledforeground='black'
+                    )
 
     def start(self):
         self.create_widgets()
         self.insert_mines()
         self.print_buttons()
+        self.open_all_buttons()
         MineSweeper.window.mainloop()
 
     def print_buttons(self):
@@ -78,10 +93,14 @@ class MineSweeper:
     def insert_mines(self):
         index_mines = self.get_mines_places()
         print(index_mines)
-        for row_btn in self.buttons:
-            for btn in row_btn:
+        count = 1
+        for i in range(1, MineSweeper.ROW + 1):
+            for j in range(1, MineSweeper.COLUMN + 1):
+                btn = self.buttons[i][j]
+                btn.number = count
                 if btn.number in index_mines:
                     btn.is_mine = True
+                count += 1
 
 
 game = MineSweeper()
